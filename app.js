1ad474cc7b5fc498f0e79ad2b4ea9b1ff8f47a70
Vincent Rohde <vincent.rohde@icloud.com>
2019-07-02 18:49:16 +0200

    add video, analytics widget

--- app.js
+++ app.js
@@ -8,21 +8,20 @@ var apos = require('apostrophe')({
     // configuring user accounts.
 
     modules: {
-
-        // Apostrophe module configuration
-
-        // Note: most configuration occurs in the respective
-        // modules' directories. See lib/apostrophe-assets/index.js for an example.
-
-        // However any modules that are not present by default in Apostrophe must at
-        // least have a minimal configuration here: `moduleName: {}`
-
-        // If a template is not found somewhere else, serve it from the top-level
-        // `views/` folder of the project
-
         'apostrophe-templates': {
-            viewsFolderFallback: path.join(__dirname, 'views')
-        }
-
+            viewsFolderFallback: path.join(__dirname, 'views'),
+            types: [
+                {
+                    name: 'post',
+                    label: 'Post'
+                },
+                {
+                    name: 'home',
+                    label: 'Home'
+                },
+            ]
+        },
+        'analytics-widgets': {},
+        'video-widgets': {},
     }
 });
