1686138020a44430909873494bcab45b6045d623
Vincent Rohde <vincent.rohde@icloud.com>
2019-07-06 13:30:53 +0200

    update analytics, curator module filtering

--- src/js/curator/index.js
+++ src/js/curator/index.js
@@ -1,110 +1,62 @@
 class Curator {
     constructor() {
         this.learningStyles = ['accommodating', 'assimilating', 'converging', 'diverging'];
-        this.score = this.createScoreArray();
     }
 
-    createScoreArray() {
-        let score = [];
-
-        this.learningStyles.forEach((item) => {
-            let scoreObject = {};
-            scoreObject.style = item;
-            scoreObject.score = 0;
-
-            score.push(scoreObject);
-        });
-
-        return score;
-    }
-
-    // set total learning style score
-    setTotalLearningStylesScore(items) {
-        this.totalLearningStylesScore = [...this.score];
-
-        let array = []
-
-        items.forEach((item) => {
-            console.log('item');
-            const { style } = item.information.items[0];
-
-            console.log(style.length);
-
-            // items with all styles should not be filtered out
-
-            if (style.length != 4 && style.length != 0) {
-                style.forEach((item) => {
-                    console.log('style: ', item);
-                });
-            }
-
-        })
-    }
-
-    filterPostContent(input, style) {
+    filterPostContent(input, preferences) {
         // set learning style for filtering
-        this.style = style;
+        this.preferences = preferences;
 
         const { body } = input.page;
         let items = body.items;
 
-        this.setTotalLearningStylesScore(items);
+        items.forEach((item, index) => {
+            const { areaContext } = item;
+            this.filterContentGroup(areaContext);
+        });
+    }
 
-        // items.forEach((item, index) => {
-        //     const { type } = item;
-        //
-        //     // remove item from render process
-        //     if (type == '') {
-        //         items.splice(index, 1);
-        //     }
-        // });
-        //
-        // items.forEach((item) => {
-        //     console.log('### item: ', item);
-        // });
+    filterContentGroup(contentGroup) {
+        const { items } = contentGroup;
+
+        this.filterForAttribute(items, 'style');
+
+        // second iteration
+        if (items.length > 1) {
+            console.log('ran into first if');
+            this.filterForAttribute(items, 'difficulty');
+        }
+
+        // third iteration
+        if (items.length > 1) {
+            console.log('ran into second if');
+            this.filterForAttribute(items, 'practicality');
+        }
+    }
+
+    filterForAttribute(items, attributeName) {
+        items.forEach((item, index) => {
+            // check if information widget exists for item
+            if (item.information) {
+                const information = item.information.items[0];
+
+                // check if items were set for information
+                if (information) {
+                    const attributeList = information[attributeName];
+
+                    if (attributeList) {
+                        if (!this.isPreferredAttribute(attributeName, attributeList)) {
+                            items.splice(index, 1);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    isPreferredAttribute(name, array) {
+        return array.filter(item => item === this.preferences[name]).length;
     }
 }
 
 module.exports = Curator;
-
-// module.exports = (function () {
-//     const learningStyles = ['accommodating', 'assimilating', 'converging', 'diverging'];
-//
-//     const createScoreObject = () => {
-//         let score = [];
-//
-//         learningStyles.forEach((item) => {
-//             let scoreObject = {};
-//             scoreObject.style = item;
-//             scoreObject.score = 0;
-//
-//             score.push(scoreObject);
-//         });
-//
-//         return score;
-//     }
-//
-//     const filterPostContent = (input) => {
-//         let score = createScoreObject();
-//
-//         const { body } = input.page;
-//
-//         body.items.forEach((item, index) => {
-//             const { type } = item;
-//
-//             // remove item from render process
-//             if (type == '') {
-//                 body.items.splice(index, 1);
-//             }
-//         });
-//
-//         // body.items.forEach((item) => {
-//         //     console.log('### item: ', item);
-//         // });
-//     }
-//
-//     return {
-//         filterPostContent
-//     }
-//
-// })();
