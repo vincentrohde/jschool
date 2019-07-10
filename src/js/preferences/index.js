5c443eb307804d6e5eaf3686b39362203b460ff9
Vincent Rohde <vincent.rohde@icloud.com>
2019-07-10 12:04:53 +0200

    implement tagPreferences for fetchPosts

--- src/js/preferences/index.js
+++ src/js/preferences/index.js
@@ -3,6 +3,34 @@ class Preferences {
         this.defaultDifficulty = 'hard';
     }
 
+    getFavoriteTags(cookieData) {
+        this.cookie = cookieData;
+        if (this.cookie.favorites) {
+            this.favoritesData = JSON.parse(this.cookie.favorites);
+            this.favoriteTagsAll = this.favoritesData.map(item => item.tags);
+
+            if (this.favoriteTagsAll) {
+                const tagsByCount = this.sortFavoriteTags();
+                const favoriteTagsByPopularity = tagsByCount.map(item => item.id);
+                let selectedFavoriteTags;
+
+                if (favoriteTagsByPopularity.length <= 3) {
+                    selectedFavoriteTags = favoriteTagsByPopularity;
+                } else {
+                    selectedFavoriteTags = favoriteTagsByPopularity.slice(0, 3);
+                }
+
+                // return final list
+                return selectedFavoriteTags;
+
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
     getPreferences(cookieData) {
         this.cookie = cookieData;
         if (this.cookie.preferenceProfile) {
@@ -20,6 +48,39 @@ class Preferences {
             console.log(prop);
             console.log(style[prop]);
         });
+    }
+
+    sortFavoriteTags() {
+        const list = [];
+
+        // count all tags by occurrence and push to list
+        this.favoriteTagsAll.forEach(favoriteTag => {
+            let occurredIndex;
+
+            const findOccurrence = (element, index) => {
+                if (element.id == favoriteTag[0]) {
+                    occurredIndex = index;
+                }
+
+                return element.id == favoriteTag[0];
+            }
+
+            list.some(findOccurrence);
+
+            if (typeof occurredIndex !== 'undefined') {
+                list[occurredIndex].count++
+            } else {
+                list.push({
+                    id: favoriteTag[0],
+                    count: 1
+                })
+            }
+        });
+
+        // sort list by count numbers
+        list.sort((a, b) => b.count - a.count);
+
+        return list;
     }
 }
 
